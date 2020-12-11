c54d41fd0072dc58c8bd637d35fb278df4b1659e
maeda-seina <ayrton19601027@gmail.com>
2020-12-11 15:28:06 +0900

    rubocopで修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'date'
 require 'optparse'
 
 date = Date.today
 month = date.mon
 year = date.year
-youbi = ["日", "月", "火", "水", "木", "金", "土"]
+youbi = %w[日 月 火 水 木 金 土]
 
 opt = OptionParser.new
-opt.on('-m value') {|v| month = v.to_i }
-opt.on('-y value') {|v| year = v.to_i }
+opt.on('-m value') { |v| month = v.to_i }
+opt.on('-y value') { |v| year = v.to_i }
 opt.parse!(ARGV)
 
 first_date = Date.new(year, month, 1)
@@ -18,10 +20,9 @@ day_youbi = Date.new(year, month, 1).wday
 
 puts ("#{month}月" "#{year}").center(20)
 puts youbi.join(' ')
-print "   " * day_youbi
-(first_date..last_date).each do |date|
-    print date.day.to_s.rjust(2) + " "
-    puts "\n" if date.saturday?
+print '   ' * day_youbi
+(first_date..last_date).each do |d|
+  print "#{d.day.to_s.rjust(2)} "
+  puts "\n" if d.saturday?
 end
 print "\n"
-
