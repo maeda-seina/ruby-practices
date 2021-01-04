696d740cf21eeae968b1603a2c4ae39dee3c97b9
maeda-seina <ayrton19601027@gmail.com>
2021-01-04 20:44:06 +0900

    lsコマンド提出

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -7,34 +7,192 @@ require 'optparse'
 require 'etc'
 
 params = ARGV.getopts('a', 'l', 'r')
-# .や..を含まないfile
-file = []
-# .や..も含んだ全てのfile
-files = []
 
-# .や..を含まないfileを作成
-Dir.glob('*') do |item|
-  file << item
-end
-p file
-
-# .や..も含んだ全てのfileを作成
-Dir.foreach('.') do |item|
-  files << item
-end
-p files
-
-def loption
-  block = 0
+# オプションなしの時の横に最大3列を維持して表示するメソッド
+def filefile
   file = []
   Dir.glob('*') do |item|
     file << item
   end
+  count = 0
+  file.each do |f|
+    count += 1
+    print "#{f}  "
+    puts "\n" if (count % 3).zero?
+  end
+  print ''
+end
 
+# rオプションの時に横に最大3列を維持して表示するメソッド
+def filefile_reverse
+  file = []
+  Dir.glob('*') do |item|
+    file << item
+  end
+  count = 0
+  file.reverse_each do |f|
+    count += 1
+    print "#{f}  "
+    puts "\n" if (count % 3).zero?
+  end
+  print ''
+end
+
+# aオプションの時に横に最大3列を維持して表示するメソッド
+def filesfiles
+  files = []
+  Dir.foreach('.') do |item|
+    files << item
+  end
+  count = 0
+  files.each do |fs|
+    count += 1
+    print "#{fs}  "
+    puts "\n" if (count % 3).zero?
+  end
+  print ''
+end
+
+# arオプションの時に横に最大3列を維持して表示するメソッド
+def filesfiles_reverse
+  files = []
+  Dir.foreach('.') do |item|
+    files << item
+  end
+  count = 0
+  files.reverse_each do |fs|
+    count += 1
+    print "#{fs}  "
+    puts "\n" if (count % 3).zero?
+  end
+  print ''
+end
+
+# .や..を含まないfileメソッド
+def file
+  file = []
+  Dir.glob('*') do |item|
+    file << item
+  end
+  file
+end
+
+# .や..も含んだ全てのfileメソッド
+def files
+  files = []
+  Dir.foreach('.') do |item|
+    files << item
+  end
+  files
+end
+
+# lオプションのブロック数の合計値を表すメソッド
+def block
+  block = 0
   file.each do |f|
     stat = File.stat(f)
     block += stat.blocks
-    puts "total #{block}"
+  end
+  puts "total #{block}"
+end
+
+# alオプションのブロック数の合計値を表すメソッド
+def blocks
+  block = 0
+  files.each do |f|
+    stat = File.stat(f)
+    block += stat.blocks
+  end
+  puts "total #{block}"
+end
+
+# lオプションのメソッド
+def loption
+  print block
+  file.each do |f|
+    stat = File.stat(f)
+    permissions = ('0%o' % stat.mode).to_i % 1000
+    per1 =
+      case permissions % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per2 =
+      case permissions / 10 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per3 =
+      case permissions / 100 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    filetype = stat.ftype
+    file =
+      case filetype
+      when 'file'
+        '-'
+      when 'directory'
+        'd'
+      when 'characterSpecial'
+        'c'
+      when 'blockSpecial'
+        'b'
+      when 'fifo'
+        'p'
+      when 'link'
+        'l'
+      when 'socket'
+        's'
+      when 'unknown'
+        'u'
+      end
+    print "#{file + per3 + per2 + per1}  "
     print "#{stat.nlink}  "
     print "#{Etc.getpwuid(File.stat(f).uid).name}  "
     print "#{Etc.getgrgid(File.stat(f).gid).name}  "
@@ -43,20 +201,321 @@ def loption
     print File.basename(f)
     print "\n"
   end
+  # 戻り値にファイルが入った配列が表示されてしまうため書いた。何かいい方法ないか。
+  print ''
 end
 
-# files.each do |fs|
-#   stats = File.stat(fs)
-# end
+# alオプションのメソッド
+def loptions
+  print blocks
+  files.each do |f|
+    stat = File.stat(f)
+    permissions = ('0%o' % stat.mode).to_i % 1000
+    per1 =
+      case permissions % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
 
-if params['a'] && params['r']
-  puts files.reverse
+    per2 =
+      case permissions / 10 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per3 =
+      case permissions / 100 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    filetype = stat.ftype
+    file =
+      case filetype
+      when 'file'
+        '-'
+      when 'directory'
+        'd'
+      when 'characterSpecial'
+        'c'
+      when 'blockSpecial'
+        'b'
+      when 'fifo'
+        'p'
+      when 'link'
+        'l'
+      when 'socket'
+        's'
+      when 'unknown'
+        'u'
+      end
+    print "#{file + per3 + per2 + per1}  "
+    print "#{stat.nlink}  "
+    print "#{Etc.getpwuid(File.stat(f).uid).name}  "
+    print "#{Etc.getgrgid(File.stat(f).gid).name}  "
+    print "#{stat.size}  "
+    print "#{stat.mtime.strftime('%m %d %H:%M')}  "
+    print File.basename(f)
+    print "\n"
+  end
+  # 戻り値にファイルが入った配列が表示されてしまうため書いた。何かいい方法ないか。
+  print ''
+end
+
+# arオプションのメソッド
+def loption_reverse
+  print block
+  file.reverse_each do |f|
+    stat = File.stat(f)
+    permissions = ('0%o' % stat.mode).to_i % 1000
+    per1 =
+      case permissions % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per2 =
+      case permissions / 10 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per3 =
+      case permissions / 100 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    filetype = stat.ftype
+    file =
+      case filetype
+      when 'file'
+        '-'
+      when 'directory'
+        'd'
+      when 'characterSpecial'
+        'c'
+      when 'blockSpecial'
+        'b'
+      when 'fifo'
+        'p'
+      when 'link'
+        'l'
+      when 'socket'
+        's'
+      when 'unknown'
+        'u'
+      end
+    print "#{file + per3 + per2 + per1}  "
+    print "#{stat.nlink}  "
+    print "#{Etc.getpwuid(File.stat(f).uid).name}  "
+    print "#{Etc.getgrgid(File.stat(f).gid).name}  "
+    print "#{stat.size}  "
+    print "#{stat.mtime.strftime('%m %d %H:%M')}  "
+    print File.basename(f)
+    print "\n"
+  end
+  # 戻り値にファイルが入った配列が表示されてしまうため書いた。何かいい方法ないか。
+  print ''
+end
+
+# alrオプションのメソッド
+def loptions_reverse
+  print blocks
+  files.reverse_each do |f|
+    stat = File.stat(f)
+    permissions = ('0%o' % stat.mode).to_i % 1000
+    per1 =
+      case permissions % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per2 =
+      case permissions / 10 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    per3 =
+      case permissions / 100 % 10
+      when 0
+        '---'
+      when 1
+        '--x'
+      when 2
+        '-w-'
+      when 3
+        '-wx'
+      when 4
+        'r--'
+      when 5
+        'r-x'
+      when 6
+        'rw-'
+      when 7
+        'rwx'
+      end
+
+    filetype = stat.ftype
+    file =
+      case filetype
+      when 'file'
+        '-'
+      when 'directory'
+        'd'
+      when 'characterSpecial'
+        'c'
+      when 'blockSpecial'
+        'b'
+      when 'fifo'
+        'p'
+      when 'link'
+        'l'
+      when 'socket'
+        's'
+      when 'unknown'
+        'u'
+      end
+    print "#{file + per3 + per2 + per1}  "
+    print "#{stat.nlink}  "
+    print "#{Etc.getpwuid(File.stat(f).uid).name}  "
+    print "#{Etc.getgrgid(File.stat(f).gid).name}  "
+    print "#{stat.size}  "
+    print "#{stat.mtime.strftime('%m %d %H:%M')}  "
+    print File.basename(f)
+    print "\n"
+  end
+  # 戻り値にファイルが入った配列が表示されてしまうため書いた。何かいい方法ないか。
+  print ''
+end
+
+if params['a'] && params['l'] && params['r']
+  puts loptions_reverse
+elsif params['r'] && params['l']
+  puts loption_reverse
+elsif params['a'] && params['r']
+  puts filesfiles_reverse
+elsif params['a'] && params['l']
+  puts loptions
 elsif params['r']
-  puts file.reverse
+  puts filefile_reverse
 elsif params['a']
-  puts files
+  puts filesfiles
 elsif params['l']
   puts loption
 else
-  puts file
+  puts filefile
 end
