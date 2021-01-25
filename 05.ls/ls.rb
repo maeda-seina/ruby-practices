3e40797eb6a2b59546c5ee2dcce037d6dca85f98
maeda-seina <ayrton19601027@gmail.com>
2021-01-25 13:07:55 +0900

    lsコマンド修正(lオプション内の条件分岐のメソッド化はまだ出来ていない)

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -9,7 +9,7 @@ require 'etc'
 params = ARGV.getopts('a', 'l', 'r')
 
 # aオプションなしの時のfile
-def file_method
+def file
   file = []
   Dir.glob('*') do |item|
     file << item
@@ -18,7 +18,7 @@ def file_method
 end
 
 # aオプションありの時のfile
-def files_method
+def file_with_a_option
   file = []
   Dir.foreach('.') do |item|
     file << item
@@ -27,39 +27,31 @@ def files_method
 end
 
 # aオプション条件分岐
-file_a_method = if params['a']
-                  files_method
-                else
-                  file_method
-                end
-
-file_a_method = file_a_method.sort
+files = params['a'] ? file_with_a_option : file
+files.sort
 
 # rオプション条件分岐
-sorted_file = if params['r']
-                file_a_method.reverse
-              else
-                file_a_method
-              end
-
-# lオプションblock数
-block = 0
-sorted_file.each do |f|
-  stat = File.stat(f)
-  block += stat.blocks
-end
+sorted_file = params['r'] ? files.reverse : files
 
 # lオプション条件分岐
 if params['l']
+  block = 0
+  sorted_file.each do |f|
+    stat = File.stat(f)
+    block += stat.blocks
+  end
   puts "total #{block}"
   sorted_file.each do |f|
     stat = File.stat(f)
     permissions = format('0%o', stat.mode).to_i % 1000
     permission = { 0 => '---', 1 => '--x', 2 => '-w-', 3 => '-wx', 4 => 'r--', 5 => 'r-x', 6 => 'rw-', 7 => 'rwx' }
+    permission_of_hundreds_place = permission[permissions / 100 % 10]
+    permission_of_tens_place = permission[permissions / 10 % 10]
+    permission_of_ones_place = permission[permissions % 10]
     filetype = stat.ftype
     file = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b', 'fifo' => 'p', 'link' => 'l', 'socket' => 's',
              'unknown' => 'u' }
-    print "#{file[filetype] + permission[permissions / 100 % 10] + permission[permissions / 10 % 10] + permission[permissions % 10]}  "
+    print "#{file[filetype] + permission_of_hundreds_place + permission_of_tens_place + permission_of_ones_place}  "
     print "#{stat.nlink} "
     print "#{Etc.getpwuid(File.stat(f).uid).name}  "
     print "#{Etc.getgrgid(File.stat(f).gid).name}  "
@@ -69,11 +61,9 @@ if params['l']
     print "\n"
   end
 else
-  count = 0
-  sorted_file.each do |f|
-    count += 1
-    print "#{f}  "
-    puts "\n" if (count % 3).zero?
+  sorted_file.each.with_index(1) do |file, index|
+    print "#{file}  "
+    puts "\n" if (index % 3).zero?
   end
   print "\n"
 end
