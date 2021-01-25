2dd967f04bd203bf94f90e7dff21f79a76694645
maeda-seina <ayrton19601027@gmail.com>
2021-01-25 15:35:26 +0900

    メソッド化挑戦

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,6 +5,13 @@ require 'optparse'
 
 params = ARGV.getopts('l')
 
+def file_print(file)
+  file = File.read(file)
+  print "     #{file.lines.count}  "
+  print "   #{file.split(/\s+/).size} "
+  print "   #{file.bytesize} "
+end
+
 # パイプラインを使った時の処理 & 標準入力受け取る処理
 if !params['l'] && ARGV.count.zero?
   file = ARGF.read
@@ -38,10 +45,7 @@ end
 
 # 引数にファイル名が1つの時の処理
 if ARGV.count == 1 && !params['l']
-  file = File.read(ARGV[0])
-  print "     #{file.lines.count}  "
-  print "   #{file.split(/\s+/).size} "
-  print "   #{file.bytesize} "
+  file_print(ARGV[0])
   print ARGV[0]
   print "\n"
 end
@@ -52,12 +56,10 @@ if ARGV.count > 1 && !params['l']
   words_sum = 0
   bytes_sum = 0
   ARGV.each do |f|
-    file = File.read(f)
-    print "     #{file.lines.count}  "
-    print "   #{file.split(/\s+/).size} "
-    print "   #{file.bytesize} "
+    file_print(f)
     print f
     print "\n"
+    file = File.read(f)
     lines_sum += file.lines.count
     words_sum += file.split(/\s+/).size
     bytes_sum += file.bytesize
