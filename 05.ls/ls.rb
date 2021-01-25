5c6c9a93838f41e6df7aa14c1be4ebb1ea522578
maeda-seina <ayrton19601027@gmail.com>
2021-01-25 14:25:55 +0900

    lオプション内の条件分岐内のメソッド化

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -8,7 +8,6 @@ require 'etc'
 
 params = ARGV.getopts('a', 'l', 'r')
 
-# aオプションなしの時のfile
 def file
   file = []
   Dir.glob('*') do |item|
@@ -17,13 +16,30 @@ def file
   file
 end
 
-# aオプションありの時のfile
 def file_with_a_option
   file = []
   Dir.foreach('.') do |item|
     file << item
   end
   file
+end
+
+def loption(file)
+  stat = File.stat(file)
+  permissions = format('0%o', stat.mode).to_i % 1000
+  permission = { 0 => '---', 1 => '--x', 2 => '-w-', 3 => '-wx', 4 => 'r--', 5 => 'r-x', 6 => 'rw-', 7 => 'rwx' }
+  permission_of_hundreds_place = permission[permissions / 100 % 10]
+  permission_of_tens_place = permission[permissions / 10 % 10]
+  permission_of_ones_place = permission[permissions % 10]
+  filetype = stat.ftype
+  file = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b', 'fifo' => 'p', 'link' => 'l', 'socket' => 's',
+           'unknown' => 'u' }
+  print "#{file[filetype] + permission_of_hundreds_place + permission_of_tens_place + permission_of_ones_place}  "
+  print "#{stat.nlink} "
+  print "#{Etc.getpwuid(stat.uid).name}  "
+  print "#{Etc.getgrgid(stat.gid).name}  "
+  print "#{stat.size}  "
+  print "#{stat.mtime.strftime('%-m %d %H:%M')}  "
 end
 
 # aオプション条件分岐
@@ -36,28 +52,14 @@ sorted_file = params['r'] ? files.reverse : files
 # lオプション条件分岐
 if params['l']
   block = 0
-  sorted_file.each do |f|
-    stat = File.stat(f)
+  sorted_file.each do |file|
+    stat = File.stat(file)
     block += stat.blocks
   end
   puts "total #{block}"
-  sorted_file.each do |f|
-    stat = File.stat(f)
-    permissions = format('0%o', stat.mode).to_i % 1000
-    permission = { 0 => '---', 1 => '--x', 2 => '-w-', 3 => '-wx', 4 => 'r--', 5 => 'r-x', 6 => 'rw-', 7 => 'rwx' }
-    permission_of_hundreds_place = permission[permissions / 100 % 10]
-    permission_of_tens_place = permission[permissions / 10 % 10]
-    permission_of_ones_place = permission[permissions % 10]
-    filetype = stat.ftype
-    file = { 'file' => '-', 'directory' => 'd', 'characterSpecial' => 'c', 'blockSpecial' => 'b', 'fifo' => 'p', 'link' => 'l', 'socket' => 's',
-             'unknown' => 'u' }
-    print "#{file[filetype] + permission_of_hundreds_place + permission_of_tens_place + permission_of_ones_place}  "
-    print "#{stat.nlink} "
-    print "#{Etc.getpwuid(File.stat(f).uid).name}  "
-    print "#{Etc.getgrgid(File.stat(f).gid).name}  "
-    print "#{stat.size}  "
-    print "#{stat.mtime.strftime('%-m %d %H:%M')}  "
-    print File.basename(f)
+  sorted_file.each do |file|
+    loption(file)
+    print File.basename(file)
     print "\n"
   end
 else
