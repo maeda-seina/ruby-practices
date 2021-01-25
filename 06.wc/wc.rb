bf67b6c8d9dfdaca84a97eda9b608bc778d47674
maeda-seina <ayrton19601027@gmail.com>
2021-01-25 18:17:01 +0900

    rjustを使って修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,17 +7,17 @@ params = ARGV.getopts('l')
 
 def print_file(file)
   file = File.read(file)
-  print "     #{file.lines.count}  "
-  print "   #{file.split(/\s+/).size} "
-  print "   #{file.bytesize} "
+  print file.lines.count.to_s.rjust(8)
+  print file.split(/\s+/).size.to_s.rjust(8)
+  print file.bytesize.to_s.rjust(8)
 end
 
 # パイプラインを使った時の処理 & 標準入力受け取る処理
 if !params['l'] && ARGV.count.zero?
   file = ARGF.read
-  print "　　   #{file.lines.count}  "
-  print "    #{file.split(/\s+/).size}  "
-  print "   #{file.bytesize}"
+  print file.lines.count.to_s.rjust(8)
+  print file.split(/\s+/).size.to_s.rjust(8)
+  print file.bytesize.to_s.rjust(8)
   print "\n"
 end
 
@@ -25,20 +25,20 @@ end
 if params['l']
   if ARGV.count == 1
     file = File.read(ARGV[0])
-    print "      #{file.lines.count} "
-    print ARGV[0]
+    print file.lines.count.to_s.rjust(8)
+    print ARGV[0].rjust(6)
     print "\n"
   elsif ARGV.count > 1
     lines_sum = 0
     ARGV.each do |f|
       file = File.read(f)
-      print "      #{file.lines.count}  "
-      print f
+      print file.lines.count.to_s.rjust(8)
+      print f.rjust(6)
       print "\n"
       lines_sum += file.lines.count
     end
-    print "      #{lines_sum}  "
-    print 'total'
+    print lines_sum.to_s.rjust(8)
+    print 'total'.rjust(6)
     print "\n"
   end
 end
@@ -46,7 +46,7 @@ end
 # 引数にファイル名が1つの時の処理
 if ARGV.count == 1 && !params['l']
   print_file(ARGV[0])
-  print ARGV[0]
+  print ARGV[0].rjust(6)
   print "\n"
 end
 
@@ -57,16 +57,16 @@ if ARGV.count > 1 && !params['l']
   bytes_sum = 0
   ARGV.each do |f|
     print_file(f)
-    print f
+    print f.rjust(6)
     print "\n"
     file = File.read(f)
     lines_sum += file.lines.count
     words_sum += file.split(/\s+/).size
     bytes_sum += file.bytesize
   end
-  print "     #{lines_sum}  "
-  print "   #{words_sum} "
-  print "   #{bytes_sum} "
-  print 'total'
+  print lines_sum.to_s.rjust(8)
+  print words_sum.to_s.rjust(8)
+  print bytes_sum.to_s.rjust(8)
+  print 'total'.rjust(6)
   print "\n"
 end
