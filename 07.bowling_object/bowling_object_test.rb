f91454b511c1663eccb63f3642cadc121e65d008
maeda-seina <ayrton19601027@gmail.com>
2021-05-02 14:37:08 +0900

    テストコード修正(動くようになった！)

--- 07.bowling_object/bowling_object_test.rb
+++ 07.bowling_object/bowling_object_test.rb
@@ -4,7 +4,7 @@ require './bowling_object'
 require 'minitest/autorun'
 
 class BowlingObjectTest < Minitest::Test
-  def frame_test
+  def test_frame
     assert_equal 5, Frame.new(2, 3).calc_frame_score
   end
 end
